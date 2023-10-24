b80c004f9142ec0cb5dece1adc2449dc0bba364b
Muhammad Alamgir Kabir <117192967+Alamgir689@users.noreply.github.com>
2023-10-24 18:15:20 +0600

    Update Count Me 1.c

--- phitron mid term exam for 1st semester/Count Me 1.c
+++ phitron mid term exam for 1st semester/Count Me 1.c
@@ -1,5 +1,32 @@
 /*
+You will be given an integer array A of size N. You need to count the number of elements that are divided by 2 and number of elements that are divided by 3. If any number is divided by both 2 and 3, 
+then consider it only for 2.
 
+Input Format
+
+First line will contain N, the size of the array
+Second line will contain the array A.
+Constraints
+
+1 <= N <= 10^5
+0 <= A[i] <= 10^9; here 0 <= i < N
+Output Format
+
+Output the number of elements that are divided by 2 first, then print the number of elements that are divided by 3.
+Sample Input 0
+
+5
+1 4 2 5 3
+Sample Output 0
+
+2 1
+Sample Input 1
+
+4
+6 9 12 15
+Sample Output 1
+
+2 2
 */
 #include<stdio.h>
 int main()
